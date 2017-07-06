7545beb327622674da6ea2b5d9c99af0ae16cdc0
Joshua Baert <jbghoring@gmail.com>
2017-07-06 16:58:16 -0500

    Finished finding the shortest path. Next need optimization.

--- helper.js
+++ helper.js
@@ -7,4 +7,19 @@ module.exports = {
 	arrayToStr(array) {
 		return array.map(line => line.join('')).join('\n')
 	},
+	
+	findWinner(pathsArray, end) {
+		for (var i = pathsArray.length - 1; i >= 0; i--) {
+			for (var j = pathsArray[i].length - 1; j >= 0; j--) {
+				if (pathsArray[i][j][0] === end.x && pathsArray[i][j][1] === end.y) {
+					return pathsArray[i].slice()
+				}
+			}
+		}
+	},
+	
+	consoleDisplay(map,path) {
+		path.map(cords=>map[cords[0]][cords[1]] = '!')
+	}
+	
 }
--- walker.js
+++ walker.js
@@ -2,78 +2,90 @@
  * Created by Joshua Baert on 7/6/2017.
  */
 
+let helper = require('./helper.js')
+
 const walker = {
 	walk(map, pathsArray, start, end) {
 		let walking = true;
 		let positions = [[start.x,start.y]]
 		
 		while (walking) {
-			walking = false;
+			console.log(helper.arrayToStr(map) + '\n');
 			positions.map((position, i, positions) => {
 				let x = position.slice(0)[0];
 				let y = position.slice(1)[0];
-				console.log('x:'+x+' y;'+y);
 				pathsArray[i].push([x,y]);
 				map[x][y] = '*';
-				let direction = walker.canWalk(map,x,y)
-				if (direction.canWalk) {
-					console.log('can walk')
-					walking = true;
-					if(direction.compass.length ===1) {
-						console.log('one direction')
-						console.log(direction.compass);
-						switch (direction.compass[0]) {
-							case 0 :
-								y+=1;
-								break;
-							case 1 :
-								x+=1;
-								break;
-							case 2 :
-								y-=1;
-								break;
-							case 3 :
-								x-=1;
-								break;
-							default :
-								console.log("didn't do anything")
-						}
-						console.log('x:'+x+' y;'+y);
-						
-					} else {
-						// more than one direction
-						walking = false
-					}
+				
+				// Check to see if we have found the end
+				if (position[0] === end.x && position[1] === end.y){
+					console.log(`We hit the end at x: ${x} & y:${y}`);
+					walking = false;
 					
+				} else{
+					
+					// See if we can move
+					let direction = walker.canWalk(map,x,y)
+					if (direction.canWalk) {
+						// if we can move do so
+						direction.compass.map((plot,index) => {
+							let x2 = x;
+							let y2 = y;
+							if (index === 0) {
+								positions[i] = walker.changeDirection(x,y,plot)
+							} else {
+								positions.push(walker.changeDirection(x,y,plot));
+								pathsArray.push(pathsArray[i].slice())
+							}
+						});
+					} else {
+						//if we can't move delete the position and path
+						positions.splice(i,1);
+						pathsArray.splice(i,1)
+					}
 				}
-				positions[i] = [x,y]
 			})
-			
 		}
-		
 	},
 	canWalk(map, x, y) {
 		let count = 0;
 		let compass = []
-		if (map[x+1][y] === '.') {
-			count+=1 ;
-			compass.push(1);
-		}
-		if (map[x-1][y] === '.') {
-			count+=1;
-			compass.push(3);
-			
-		}
 		if (map[x][y+1] === '.') {
 			count+=1;
 			compass.push(0);
+		}
+		if (map[x+1][y] === '.') {
+			count+=1 ;
+			compass.push(1);
 		}
 		if (map[x][y-1] === '.') {
 			count+=1;
 			compass.push(2);
 		}
+		if (map[x-1][y] === '.') {
+			count+=1;
+			compass.push(3);
+		}
 		return {canWalk: count >= 1, compass: compass}
-	}
+	},
+	
+	changeDirection(x,y,test){
+		switch (test) {
+			case 0 :
+				y+=1;
+				break;
+			case 1 :
+				x+=1;
+				break;
+			case 2 :
+				y-=1;
+				break;
+			case 3 :
+				x-=1;
+				break;
+		}
+		return [x,y]
+	},
 	
 };
 
