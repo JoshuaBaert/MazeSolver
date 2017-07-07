36e18f1b0e9cb235e36d1abb5bf2fd8f0c81e4e4
Joshua Baert <jbghoring@gmail.com>
2017-07-07 13:09:51 -0500

    Getting CodeMirror

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,23 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-// import {CodemirrorModule} from 'ng2-codemirror';
 
 import { AppComponent } from './app.component';
-import {CodemirrorComponent} from 'ng2-codemirror';
 import { InputComponent } from './input/input.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CodemirrorComponent,
     InputComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    // CodemirrorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- public/src/app/input/input.component.ts
+++ public/src/app/input/input.component.ts
@@ -4,29 +4,11 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-input',
   styleUrls: ['./input.component.scss'],
   template: `
-    <codemirror [(ngModel)]="code"
-    [config]="config">
-      
-    </codemirror>
   `
 })
 export class InputComponent implements OnInit {
-  code;
-  config = {
-
-  };
 
   constructor() {
-    this.code = `
-##########
-#A...#...#
-#.#.##.#.#
-#.#.##.#.#
-#.#....#B#
-#.#.##.#.#
-#....#...#
-##########
-`
   }
 
   ngOnInit() {
