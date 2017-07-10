831e631a2f9ac16cd0ba65f64a440b6d85ae783c
Joshua Baert <jbghoring@gmail.com>
2017-07-10 15:42:45 -0500

    Cleaning up code and Adding notes

--- test.js
+++ test.js
@@ -2,42 +2,4 @@
  * Created by Joshua Baert on 7/5/2017.
  */
 
-let x1 = true;
-let y1 = false;
-let x2 = true;
-let y2 = false;
 
-console.log(( x1 && y1) || (x2 && y2))
-
-/*
-
-let hurry = ["#"];
-console.log(typeof hurry)
-hurry.unshift('#');
-console.log(typeof hurry)
-hurry.push('#')
-console.log(typeof hurry)
-*/
-
-
-/*
-let compass = [3]
-let x =2
-let y =2
-
-switch (compass[0]) {
-	case 0 :
-		y+=1;
-		break;
-	case 1 :
-		x+=1;
-		break;
-	case 2 :
-		y-=1;
-		break;
-	case 3 :
-		x-=1;
-		break;
-	default :
-		console.log("didn't do anything")
-}*/
